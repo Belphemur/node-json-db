c3f4aa1bf192d93d9acf1c7e07125ad1509a0ec2
Antoine Aflalo <antoineaf@admincmd.com>
2014-07-12 16:30:49 +0300

    [FIRST VERSION] First version of the module.

---
+++ JsonDB.js
@@ -0,0 +1,151 @@
+var FS = require('fs');
+var events = require('events');
+var util = require('util');
+var JsonUtils = require("./lib/utils");
+var DBParentData = require("./lib/DBParentData");
+
+var JsonDB = function (filename, saveOnPush) {
+
+    events.EventEmitter.call(this);
+    this.filename = filename;
+
+    if (!JsonUtils.strEndWith(filename, ".json")) {
+        this.filename += ".json";
+    }
+    var self = this;
+    this.loaded = false;
+    this.data = {};
+    if (!FS.existsSync(this.filename)) {
+        self.save(true);
+        self.loaded = true;
+        util.log("[JsonDB] DataBase " + self.filename + " created.");
+    }
+    this.saveOnPush = saveOnPush;
+
+    return this;
+};
+util.inherits(JsonDB, events.EventEmitter);
+JsonDB.prototype.processDataPath = function (dataPath) {
+    if (dataPath === undefined || !dataPath.trim()) {
+        this.emit('error', new Error("The Data Path can't be empty"));
+        return;
+    }
+    if (dataPath == "/") {
+        return [];
+    }
+    var path = dataPath.split("/");
+    path.shift();
+    return path;
+}
+
+JsonDB.prototype.getParentData = function (dataPath, create) {
+    var path = this.processDataPath(dataPath);
+    if (path === undefined) {
+        return;
+    }
+    var last = path.pop();
+    return new DBParentData(last, this._getData(path, create), this);
+}
+
+JsonDB.prototype.getData = function (dataPath) {
+    var path = this.processDataPath(dataPath);
+    if (path === undefined) {
+    }
+    return this._getData(path);
+}
+
+JsonDB.prototype._getData = function (dataPath, create) {
+    this.load();
+
+    create = create || false;
+    if (dataPath.length == 0) {
+        return this.data;
+    }
+    var data = this.data;
+    for (var i in dataPath) {
+        if (dataPath.hasOwnProperty(i)) {
+            var property = dataPath[i];
+            if (data.hasOwnProperty(property)) {
+                data = data[property];
+            } else if (create) {
+                data[property] = {};
+                data = data[property];
+            } else {
+                var error = new Error("Can't find dataPath: " + dataPath + ". Stopped at " + property);
+                this.emit('error', error);
+            }
+        }
+    }
+    return data;
+
+}
+JsonDB.prototype.push = function (dataPath, data, override) {
+    override = override === undefined ? true : override;
+    var dbData = this.getParentData(dataPath, true);
+    if (!dbData.data) {
+        this.emit('error', new Error("Data not found"))
+        return;
+    }
+    var toSet = data;
+    if (!override) {
+        if (Array.isArray(data)) {
+            if (!Array.isArray(dbData.getData())) {
+                this.emit('error', new Error("Can't merge another type of data with an Array"));
+                return;
+            }
+            toSet = dbData.getData().concat(data);
+        } else if (data === Object(data)) {
+            if (Array.isArray(dbData.getData())) {
+                this.emit('error', new Error("Can't merge an Array with an Object"));
+                return;
+            }
+            toSet = JsonUtils.mergeObject(dbData.getData(), data);
+        }
+    }
+    dbData.setData(toSet);
+
+    if (this.saveOnPush) {
+        this.save();
+    }
+}
+JsonDB.prototype.delete = function (dataPath) {
+    var dbData = this.getParentData(dataPath, true);
+    if (!dbData.data) {
+        return;
+    }
+    dbData.delete();
+}
+
+JsonDB.prototype.load = function () {
+    if (this.loaded) {
+        return;
+    }
+    try {
+        var data = FS.readFileSync(this.filename, 'utf8');
+        this.data = JSON.parse(data);
+        this.loaded = true;
+        util.log("[JsonDB] DataBase " + this.filename + " loaded.");
+    } catch (err) {
+        var error = new Error("Can't Load Database: " + err);
+        error.inner = err;
+        this.emit('error', error);
+    }
+}
+JsonDB.prototype.save = function (force) {
+    force = force || false;
+    if (!force && !this.loaded) {
+        this.emit('error', new Error("DataBase not loaded. Can't write"));
+    }
+    var data = "";
+    try {
+        data = JSON.stringify(this.data);
+        FS.writeFileSync(this.filename, data, 'utf8');
+    } catch (err) {
+        var error = new Error("Can't save the database: " + err);
+        error.inner = err;
+        this.emit('error', error);
+    }
+
+}
+
+exports = module.exports = JsonDB;
---
+++ test/test.js
@@ -0,0 +1,89 @@
+var assert = require("assert");
+var JsonDB = require("../JsonDB.js");
+var fs = require('fs');
+var util = require('util');
+
+var testFile1 = "test_file1";
+var testFile2 = "test_file2";
+describe('JsonDB', function () {
+    describe('Initialisation', function () {
+        var db = new JsonDB(testFile1, true);
+        db.on('error', function (error) {
+            throw error;
+        });
+        it('should create the JSON File', function (done) {
+            fs.exists(testFile1 + ".json", function (exists) {
+                assert(exists, "File should exits");
+                done();
+            });
+
+        })
+
+        it('should set en empty root', function () {
+            assert.equal("{}", JSON.stringify(db.getData("/")));
+        })
+
+    })
+    describe('Data Management', function () {
+
+        var db = new JsonDB(testFile2, true);
+
+        it('should store the data at the root', function () {
+            var object = {test: {test: "test"}};
+            db.push("/", object);
+            assert.strictEqual(db.getData("/"), object);
+        })
+        it('should override the data at the root', function () {
+            var object = {test: "test"};
+            db.push("/", object);
+            assert.strictEqual(db.getData("/"), object);
+        })
+        it('should merge the data at the root', function () {
+            var object = {test: {test: ['Okay']}};
+            db.push("/", object);
+            var data = db.getData("/");
+            assert(object, data);
+            var object = {test: {test: ['Perfect'], okay: "test"}}
+            db.push("/", object, false);
+            assert.equal(JSON.stringify(db.getData("/")), '{\"test\":{\"test\":[\"Okay\",\"Perfect\"],\"okay\":\"test\"}}');
+        })
+        it('should return right data for datapath', function () {
+            db = new JsonDB(testFile2, true);
+            assert.equal(JSON.stringify(db.getData("/test")), '{\"test\":[\"Okay\",\"Perfect\"],\"okay\":\"test\"}');
+
+        })
+
+        it('should override only the data at datapath', function () {
+            var data = ['overriden'];
+            db.push("/test/test", data);
+            assert.strictEqual(db.getData("/test/test"), data);
+        })
+        it('should merge the data at datapath', function () {
+            var data = ['test2'];
+            db.push("/test/test", data, false);
+            assert.equal(JSON.stringify(db.getData("/test/test")), '[\"overriden\",\"test2\"]');
+        })
+
+        it('should throw an Error when merging Object with Array', function () {
+            assert.throws(function () {
+                db.push("/test/test", {myTest: "test"}, false);
+            })
+        })
+
+        it('should delete the data', function () {
+            db.delete("/test/test");
+            assert.throws(function () {
+                db.getData("/test/test");
+            })
+        })
+
+
+    });
+    describe('Cleanup', function () {
+        it('should remove the test files', function () {
+            fs.unlinkSync(testFile1 + ".json");
+            fs.unlinkSync(testFile2 + ".json");
+        });
+    });
+
+});
