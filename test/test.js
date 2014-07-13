7013c12e15008147eee9efb4b8eecfac1f3d6a19
Antoine Aflalo <antoineaf@admincmd.com>
2014-07-13 18:15:37 +0300

    [MOD] Use expect as test library instead of assert Easier to test the new Exception created.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-var assert = require("assert");
+var expect = require("expect.js");
 var JsonDB = require("../JsonDB.js");
+var DatabaseError = require("../lib/Errors").DatabaseError;
+var DataError = require("../lib/Errors").DataError;
+
 var fs = require('fs');
 var util = require('util');
 
@@ -12,28 +15,29 @@ describe('JsonDB', function () {
 
         it('should create the JSON File', function (done) {
             fs.exists(testFile1 + ".json", function (exists) {
-                assert(exists, "File should exits");
+                expect(exists).to.be.ok();
                 done();
             });
 
         })
 
         it('should set en empty root', function () {
-            assert.equal("{}", JSON.stringify(db.getData("/")));
+            expect(JSON.stringify(db.getData("/"))).to.eql("{}");
         })
 
-        it('should return a loading error', function () {
+        it('should return a DatabaseError when loading faulty file', function () {
             db = new JsonDB(faulty, true);
-            assert.throws(function () {
-                db.getData("/");
-            });
+            expect(function (args) {
+                db.getData(args);
+            }).withArgs("/").to.throwException(function (e) {
+                    expect(e).to.be.a(DatabaseError);
+                });
 
         })
-        it('should return a save error', function () {
-            assert.throws(function () {
-                db.save();
+        it('should return a DatabaseError when saving without successful loading.', function () {
+            expect(db.save).to.throwException(function (e) {
+                expect(e).to.be.a(DatabaseError);
             });
-
         })
 
     })
@@ -44,68 +48,77 @@ describe('JsonDB', function () {
         it('should store the data at the root', function () {
             var object = {test: {test: "test"}};
             db.push("/", object);
-            assert.strictEqual(db.getData("/"), object);
+            expect(db.getData("/")).to.be(object);
         })
         it('should override the data at the root', function () {
             var object = {test: "test"};
             db.push("/", object);
-            assert.strictEqual(db.getData("/"), object);
+            expect(db.getData("/")).to.be(object);
         })
         it('should merge the data at the root', function () {
             var object = {test: {test: ['Okay']}};
             db.push("/", object);
             var data = db.getData("/");
-            assert.strictEqual(data, object);
+            expect(data).to.be(object);
             object = {test: {test: ['Perfect'], okay: "test"}}
             db.push("/", object, false);
-            assert.equal(JSON.stringify(db.getData("/")), '{\"test\":{\"test\":[\"Okay\",\"Perfect\"],\"okay\":\"test\"}}');
+            expect(JSON.stringify(db.getData("/"))).to.eql('{\"test\":{\"test\":[\"Okay\",\"Perfect\"],\"okay\":\"test\"}}');
         })
         it('should return right data for datapath', function () {
             db = new JsonDB(testFile2, true);
-            assert.equal(JSON.stringify(db.getData("/test")), '{\"test\":[\"Okay\",\"Perfect\"],\"okay\":\"test\"}');
-
+            expect(JSON.stringify(db.getData("/test"))).to.eql('{\"test\":[\"Okay\",\"Perfect\"],\"okay\":\"test\"}');
         })
 
         it('should override only the data at datapath', function () {
-            var data = ['overriden'];
-            db.push("/test/test", data);
-            assert.strictEqual(db.getData("/test/test"), data);
+            var object = ['overriden'];
+            db.push("/test/test", object);
+            expect(db.getData("/test/test")).to.be(object);
         })
         it('should merge the data at datapath', function () {
-            var data = ['test2'];
-            db.push("/test/test", data, false);
-            assert.equal(JSON.stringify(db.getData("/test/test")), '[\"overriden\",\"test2\"]');
+            var object = ['test2'];
+            db.push("/test/test", object, false);
+            expect(JSON.stringify(db.getData("/test/test"))).to.eql('[\"overriden\",\"test2\"]');
         })
 
         it('should create the tree to reach datapath', function () {
-            var data = ['test2'];
-            db.push("/my/tree/is/awesome", data, false);
-            assert.equal(JSON.stringify(db.getData("/my/tree/is/awesome")), '[\"test2\"]');
+            var object = ['test2'];
+            db.push("/my/tree/is/awesome", object, false);
+            expect(JSON.stringify(db.getData("/my/tree/is/awesome"))).to.eql('[\"test2\"]');
         })
         it('should throw an Error when merging Object with Array', function () {
-            assert.throws(function () {
-                db.push("/test/test", {myTest: "test"}, false);
-            })
+            expect(function (path, data, override) {
+                db.push(path, data, override);
+            }).withArgs("/test/test", {myTest: "test"}, false).to.throwException(function (e) {
+                    expect(e).to.be.a(DataError);
+                });
         })
 
         it('should throw an Error when merging Array with Object', function () {
-            assert.throws(function () {
-                db.push("/test", ['test'], false);
-            })
+            expect(function (path, data, override) {
+                db.push(path, data, override);
+            }).withArgs("/test", ['test'], false).to.throwException(function (e) {
+                    expect(e).to.be.a(DataError);
+                });
         })
 
 
         it('should throw an Error when asking for empty datapath', function () {
-            assert.throws(function () {
-                db.getData("");
-            })
+            expect(function (args) {
+                db.getData(args);
+            }).withArgs("").to.throwException(function (e) {
+                    expect(e).to.be.a(DataError);
+                });
+
         })
 
         it('should delete the data', function () {
             db.delete("/test/test");
-            assert.throws(function () {
-                db.getData("/test/test");
-            })
+            expect(function (args) {
+                db.getData(args);
+            }).withArgs("/test/test").to.throwException(function (e) {
+                    expect(e).to.be.a(DataError);
+                });
+
         })
 
 
