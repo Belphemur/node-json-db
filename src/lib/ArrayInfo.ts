c463e488730f0f44f0f7871e542340a881ea98df
Sid Blommers <sidblommerswork@gmail.com>
2022-06-25 19:09:17 -0500

    Added the ability for nested arrays.

--- src/lib/ArrayInfo.ts
+++ src/lib/ArrayInfo.ts
@@ -1,121 +1,169 @@
-import {DataError} from "./Errors"
-import {KeyValue} from "./Utils"
+import { DataError } from './Errors'
+import { KeyValue } from './Utils'
 
 function isInt(value: any) {
-    return !isNaN(value) &&
-        Number(value) == value && !isNaN(parseInt(value, 10))
+  return !isNaN(value) && Number(value) == value && !isNaN(parseInt(value, 10))
 }
 
-export const arrayRegex = () => /^([\.0-9a-zA-Z_$\-][0-9a-zA-Z_\-$\.]*)\[((?!(\]|\[)).*|)\]$/gm
+export const arrayRegex = () => /^([\.0-9a-zA-Z_$\-][0-9a-zA-Z_\-$\.]*)(.*)/gm
+export const arrayIndiciesRegex = /\[(.?|.+?)\]/g
+export const malformedArrayRegex = /\]|\[/g
 const regexCache = {} as KeyValue
 
 export class ArrayInfo {
-    readonly property: string
-    readonly index: number = 0
-    readonly append: boolean = false
+  readonly property: string
+  readonly index: number = 0
+  readonly append: boolean = false
+  readonly indicies = [] as any[]
 
+  constructor(property: string, indicies: any[]) {
+    this.property = property
+    const index = indicies[0] ?? 0
+    this.append = index === '' || indicies[indicies.length - 1] === ''
+    this.indicies = indicies
+    if (isInt(index)) {
+      this.index = parseInt(index)
+    } else if (!this.append) {
+      throw new DataError('Only numerical values accepted for array index', 200)
+    }
+  }
 
-    constructor(property: string, index: any) {
-        this.property = property
-        this.append = index === ""
-        if (isInt(index)) {
-            this.index = parseInt(index)
-        } else if (!this.append) {
-            throw new DataError("Only numerical values accepted for array index", 200)
-        }
+  /**
+   * Check if the property want to access an Array
+   * @param property
+   */
+  public static processArray(property?: string): ArrayInfo | null {
+    if (typeof property === 'undefined') {
+      return null
     }
 
-    /**
-     * Check if the property want to access an Array
-     * @param property
-     */
-    public static processArray(property?: string): ArrayInfo | null {
-        if (typeof property === 'undefined') {
-            return null
-        }
-
-        if (regexCache[property]) {
-            return regexCache[property]
-        }
-
-        const arrayIndexRegex = arrayRegex()
-        const match = arrayIndexRegex.exec(property.trim())
-        if (match != null) {
-            return (regexCache[property] = new ArrayInfo(match[1], match[2]))
-        }
-        return null
+    if (regexCache[property]) {
+      return regexCache[property]
     }
 
-    /**
-     * Get the index for the array
-     * @param data
-     * @param avoidProperty
-     */
-    public getIndex(data: KeyValue, avoidProperty?: boolean): number {
-        if (avoidProperty === undefined) {
-            avoidProperty = false
+    const arrayIndexRegex = arrayRegex()
+    const match = arrayIndexRegex.exec(property.trim())
+    if (match != null && match[2]) {
+      const propertyName = match[1]
+      const nestedArrayMatches = match[2].toString()
+      const nestedArrayIndicies = [
+        ...nestedArrayMatches.matchAll(arrayIndiciesRegex),
+      ].map((match) => match[1])
+      let malformedArray = false
+      nestedArrayIndicies.forEach((value) => {
+        if (value.match(malformedArrayRegex)) {
+          malformedArray = true
         }
-
-        if (this.append) {
-            return -1
-        }
-
-        const index = this.index
-        if (index == -1) {
-            const dataIterable = avoidProperty ? data : data[this.property]
-
-            if (dataIterable.length === 0) {
-                return 0
-            }
-            return dataIterable.length - 1
-        }
-        return index
+      })
+      if (!malformedArray && nestedArrayIndicies.length > 0) {
+        return (regexCache[property] = new ArrayInfo(
+          propertyName,
+          nestedArrayIndicies
+        ))
+      }
     }
 
-    /**
-     * Get the Data
-     * @param data
-     */
-    public getData(data: KeyValue): any {
-        if (this.append) {
-            throw new DataError("Can't get data when appending", 100)
-        }
-        const index = this.getIndex(data)
-        return data[this.property][index]
+    return null
+  }
+
+  /**
+   * Get the index for the array
+   * @param data
+   * @param avoidProperty
+   */
+  public getIndex(data: KeyValue, avoidProperty?: boolean): number {
+    if (avoidProperty === undefined) {
+      avoidProperty = false
     }
 
-    /**
-     * Set the data for the array
-     * @param data
-     * @param value
-     */
-    public setData(data: KeyValue, value: any): void {
-        if (this.append) {
-            data[this.property].push(value)
-        } else {
-            const index = this.getIndex(data)
-            data[this.property][index] = value
-        }
+    if (this.append) {
+      return -1
     }
 
-    /**
-     * Delete the index from the array
-     * @param data
-     */
-    public delete(data: KeyValue): void {
-        if (this.append) {
-            throw new DataError("Can't delete an appended data", 10)
-        }
-        const index = this.getIndex(data)
-        data[this.property].splice(index, 1)
-    }
+    const index = this.index
+    if (index == -1) {
+      const dataIterable = avoidProperty ? data : data[this.property]
 
-    /**
-     * Check if the ArrayInfo is valid for the given data
-     * @param data
-     */
-    public isValid(data: KeyValue): boolean {
-        const index = this.getIndex(data)
-        return data[this.property].hasOwnProperty(index)
+      if (dataIterable.length === 0) {
+        return 0
+      }
+      return dataIterable.length - 1
     }
+    return index
+  }
+
+  /**
+   * Get the Data
+   * @param data
+   */
+  public getData(data: KeyValue): any {
+    if (this.append) {
+      throw new DataError("Can't get data when appending", 100)
+    }
+    const index = this.getIndex(data)
+    return data[this.property][index]
+  }
+
+  public getMultiDimensionalData(data: KeyValue): any {
+    if (this.append) {
+      throw new DataError("Can't get data when appending", 100)
+    }
+    this.indicies.forEach((index) => {
+      data = data[index]
+    })
+    return data
+  }
+
+  /**
+   * Set the data for the array
+   * @param data
+   * @param value
+   */
+  public setData(data: KeyValue, value: any): void {
+    if (this.append) {
+      let dataLocationToAppendTo = data[this.property]
+      this.indicies.forEach((index) => {
+        if (index === '') {
+          return
+        }
+        dataLocationToAppendTo = dataLocationToAppendTo[+index]
+      })
+      dataLocationToAppendTo.push(value)
+    } else {
+      const index = this.getIndex(data)
+      data[this.property][index] = value
+    }
+  }
+
+  /**
+   * Delete the index from the array
+   * @param data
+   */
+  public delete(data: KeyValue): void {
+    if (this.append) {
+      throw new DataError("Can't delete an appended data", 10)
+    }
+    let indexToRemove = this.getIndex(data)
+    let tempData = data[this.property]
+    if (this.indicies.length > 0) {
+      indexToRemove = this.indicies[this.indicies.length - 1]
+      for (let i = 0; i < this.indicies.length - 1; i++) {
+        tempData = tempData[i]
+      }
+    }
+    tempData.splice(+indexToRemove, 1)
+  }
+
+  /**
+   * Check if the ArrayInfo is valid for the given data
+   * @param data
+   */
+  public isValid(data: KeyValue): boolean {
+    const index = this.getIndex(data)
+    return data[this.property].hasOwnProperty(index)
+  }
+
+  public isMultiDimensional() {
+    return this.indicies.length > 1
+  }
 }
--- test/04-array-utils.test.ts
+++ test/04-array-utils.test.ts
@@ -2,94 +2,158 @@ import { JsonDB } from '../src/JsonDB'
 import * as fs from 'fs'
 
 describe('Array Utils', () => {
-    const db = new JsonDB('test/recipe', true, true)
-    describe('get number of item in an array', () => {
-        test('should have the correct count of an array', () => {
-            const recipe_1 = {id: 65464646155, name: "Cheesecake", category: "Dessert"};
-            const recipe_2 = {id: 78687873783, name: "Cheeseburger", category: "Dish"};
-            const recipe_3 = {id: 12335373873, name: "Soup", category: "Starter"};
-            db.push("/recipes[0]", recipe_1, true);
-            db.push("/recipes[1]", recipe_2, true);
-            db.push("/recipes[2]", recipe_3, true);
+  const db = new JsonDB('test/recipe', true, true)
+  describe('get number of item in an array', () => {
+    test('should have the correct count of an array', () => {
+      const recipe_1 = {
+        id: 65464646155,
+        name: 'Cheesecake',
+        category: 'Dessert',
+      }
+      const recipe_2 = {
+        id: 78687873783,
+        name: 'Cheeseburger',
+        category: 'Dish',
+      }
+      const recipe_3 = { id: 12335373873, name: 'Soup', category: 'Starter' }
+      db.push('/recipes[0]', recipe_1, true)
+      db.push('/recipes[1]', recipe_2, true)
+      db.push('/recipes[2]', recipe_3, true)
 
-            expect(db.count("/recipes")).toBe(3);
-        })
-
+      expect(db.count('/recipes')).toBe(3)
     })
-    describe('get index of item in an array', () => {
-        test('should get the index of the current value', () => {
-            const recipe_1 = {id: "65464646155", name: "Cheesecake", category: "Dessert"};
-            const recipe_2 = {id: "78687873783", name: "Gratin", category: "Dish"};
-            const recipe_3 = {id: "12335373873", name: "Soupe", category: "Starter"};
-            db.push("/recipes[0]", recipe_1, true);
-            db.push("/recipes[1]", recipe_2, true);
-            db.push("/recipes[2]", recipe_3, true);
+  })
+  describe('get index of item in an array', () => {
+    test('should get the index of the current value', () => {
+      const recipe_1 = {
+        id: '65464646155',
+        name: 'Cheesecake',
+        category: 'Dessert',
+      }
+      const recipe_2 = { id: '78687873783', name: 'Gratin', category: 'Dish' }
+      const recipe_3 = {
+        id: '12335373873',
+        name: 'Soupe',
+        category: 'Starter',
+      }
+      db.push('/recipes[0]', recipe_1, true)
+      db.push('/recipes[1]', recipe_2, true)
+      db.push('/recipes[2]', recipe_3, true)
 
-            expect(db.getIndex("/recipes", "65464646155")).toBe(0);
-            expect(db.getIndex("/recipes", "78687873783")).toBe(1);
-            expect(db.getIndex("/recipes", "12335373873")).toBe(2);
+      expect(db.getIndex('/recipes', '65464646155')).toBe(0)
+      expect(db.getIndex('/recipes', '78687873783')).toBe(1)
+      expect(db.getIndex('/recipes', '12335373873')).toBe(2)
 
-            db.delete("/recipes[" + db.getIndex("/recipes", "78687873783") + "]");
+      db.delete('/recipes[' + db.getIndex('/recipes', '78687873783') + ']')
 
-            expect(db.getIndex("/recipes", "65464646155")).toBe(0);
-            expect(db.getIndex("/recipes", "12335373873")).toBe(1);
-        })
-        
-        test('should get the index of the current value with anything but id', () => {
-            const recipe_1 = {test: "65464646155", name: "Cheesecake", category: "Dessert"};
-            const recipe_2 = {test: "78687873783", name: "Gratin", category: "Dish"};
-            const recipe_3 = {test: "12335373873", name: "Soupe", category: "Starter"};
-            db.push("/recipes[0]", recipe_1, true);
-            db.push("/recipes[1]", recipe_2, true);
-            db.push("/recipes[2]", recipe_3, true);
-
-            expect(db.getIndex("/recipes", "65464646155", "test")).toBe(0);
-            expect(db.getIndex("/recipes", "78687873783", "test")).toBe(1);
-            expect(db.getIndex("/recipes", "12335373873", "test")).toBe(2);
-        })
-        test('should get the index of the current value with anything but numerical', () => {
-            const recipe_1 = {test: 65464646155, name: "Cheesecake", category: "Dessert"};
-            const recipe_2 = {test: 78687873783, name: "Gratin", category: "Dish"};
-            const recipe_3 = {test: 12335373873, name: "Soupe", category: "Starter"};
-            db.push("/recipes[0]", recipe_1, true);
-            db.push("/recipes[1]", recipe_2, true);
-            db.push("/recipes[2]", recipe_3, true);
-
-            expect(db.getIndex("/recipes", 65464646155, "test")).toBe(0);
-            expect(db.getIndex("/recipes", 78687873783, "test")).toBe(1);
-            expect(db.getIndex("/recipes", 12335373873, "test")).toBe(2);
-        })
-
-        test('should get the index of an array with string', () => {
-
-            db.push("/indexValue[]", "abc", true);
-            db.push("/indexValue[]", "def", true);
-            db.push("/indexValue[]", "gh", true);
-
-            expect(db.getIndexValue("/indexValue", "abc")).toBe(0);
-            expect(db.getIndexValue("/indexValue", "def")).toBe(1);
-            expect(db.getIndexValue("/indexValue", "gh")).toBe(2);
-        })
-
+      expect(db.getIndex('/recipes', '65464646155')).toBe(0)
+      expect(db.getIndex('/recipes', '12335373873')).toBe(1)
     })
-    describe('Nested array', () => {
-        test('get data of a nested array', () => {
-            db.push("/nested/0/array", [["test", "test2"]], true);
-            expect(db.getData("/nested/0/array[0][0]")).toBe("test");
-        })
-        test('append data to a nested array', () => {
-            db.push("/nested/1/array", [["test", "test2"]], true);
-            db.push("/nested/1/array[0][]", "test3", true);
-            expect(db.getData("/nested/array[0][2]")).toBe("test3");
-        })
-        test('get data of a multiple nested array', () => {
-            db.push("/nested/0/array", [[["test", "test2"]]], true);
-            expect(db.getData("/nested/0/array[0][0][0]")).toBe("test");
-        })
+
+    test('should get the index of the current value with anything but id', () => {
+      const recipe_1 = {
+        test: '65464646155',
+        name: 'Cheesecake',
+        category: 'Dessert',
+      }
+      const recipe_2 = {
+        test: '78687873783',
+        name: 'Gratin',
+        category: 'Dish',
+      }
+      const recipe_3 = {
+        test: '12335373873',
+        name: 'Soupe',
+        category: 'Starter',
+      }
+      db.push('/recipes[0]', recipe_1, true)
+      db.push('/recipes[1]', recipe_2, true)
+      db.push('/recipes[2]', recipe_3, true)
+
+      expect(db.getIndex('/recipes', '65464646155', 'test')).toBe(0)
+      expect(db.getIndex('/recipes', '78687873783', 'test')).toBe(1)
+      expect(db.getIndex('/recipes', '12335373873', 'test')).toBe(2)
     })
-    describe('Cleanup', () => {
-        test('should remove the test files', () => {
-            fs.unlinkSync("test/recipe.json")
-        })
+    test('should get the index of the current value with anything but numerical', () => {
+      const recipe_1 = {
+        test: 65464646155,
+        name: 'Cheesecake',
+        category: 'Dessert',
+      }
+      const recipe_2 = { test: 78687873783, name: 'Gratin', category: 'Dish' }
+      const recipe_3 = {
+        test: 12335373873,
+        name: 'Soupe',
+        category: 'Starter',
+      }
+      db.push('/recipes[0]', recipe_1, true)
+      db.push('/recipes[1]', recipe_2, true)
+      db.push('/recipes[2]', recipe_3, true)
+
+      expect(db.getIndex('/recipes', 65464646155, 'test')).toBe(0)
+      expect(db.getIndex('/recipes', 78687873783, 'test')).toBe(1)
+      expect(db.getIndex('/recipes', 12335373873, 'test')).toBe(2)
     })
+
+    test('should get the index of an array with string', () => {
+      db.push('/indexValue[]', 'abc', true)
+      db.push('/indexValue[]', 'def', true)
+      db.push('/indexValue[]', 'gh', true)
+
+      expect(db.getIndexValue('/indexValue', 'abc')).toBe(0)
+      expect(db.getIndexValue('/indexValue', 'def')).toBe(1)
+      expect(db.getIndexValue('/indexValue', 'gh')).toBe(2)
+    })
+  })
+  describe('Nested array', () => {
+    test('get data of a nested array', () => {
+      db.push('/nested/0/array', [['test', 'test2']], true)
+      expect(db.getData('/nested/0/array[0][0]')).toBe('test')
+    })
+    test('append data to a nested array', () => {
+      db.push('/nested/1/array', [['test', 'test2']], true)
+      db.push('/nested/1/array[0][]', 'test3', true)
+      expect(db.getData('/nested/1/array[0][2]')).toBe('test3')
+    })
+    test('get data of a multiple nested array', () => {
+      db.push('/nested/0/array', [[['test', 'test2']]], true)
+      expect(db.getData('/nested/0/array[0][0][0]')).toBe('test')
+    })
+    test('should delete array index at specified position', () => {
+      db.push('/nested/0/array', [
+        ['abc', 'def'],
+        ['hij', 'klmn'],
+      ])
+      db.delete('/nested/0/array[0][0]')
+      expect(db.getData('/nested/0/array[0][0]')).toBe('def')
+    })
+  })
+  describe('Nested array regex', () => {
+    // Test added to play with the regex
+    test('should find index for nested array', () => {
+      const arrayRegex = () => /^([\.0-9a-zA-Z_$\-][0-9a-zA-Z_\-$\.]*)(.*)/gm
+      const arrayIndexRegex = arrayRegex()
+      const match = arrayIndexRegex.exec('array[1][2]')
+      let property = ''
+      let nestedArrayIndicies = [] as any[]
+      if (match != null && match[2]) {
+        // Match 1 = property name
+        // Match 2 = Array or Nested Arrays
+        property = match[1]
+        const nestedArrayMatches = match[2].toString()
+        nestedArrayIndicies = [
+          // matchAll to support [5] and []
+          ...nestedArrayMatches.matchAll(/\[(.?|.+?)\]/g),
+        ].map((match) => match[1])
+      }
+      expect(property).toBe('array')
+      expect(nestedArrayIndicies[0]).toEqual('1')
+      expect(nestedArrayIndicies[1]).toEqual('2')
+    })
+  })
+  describe('Cleanup', () => {
+    test('should remove the test files', () => {
+      fs.unlinkSync('test/recipe.json')
+    })
+  })
 })
