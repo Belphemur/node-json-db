c8f264b1f71d4e1d0c72fe9954134a0af7c4d10f
Antoine Aflalo <git@aaflalo.me>
2022-08-01 16:59:15 -0400

    feat(Adapter): Let the user decide what adapter to use if they want to tweak the inner working
    Fixes #448

--- src/lib/JsonDBConfig.ts
+++ src/lib/JsonDBConfig.ts
@@ -1,31 +1,53 @@
 import * as path from "path";
+import {IAdapter} from "../adapter/IAdapter";
+import {JsonAdapter} from "../adapter/data/JsonAdapter";
+import {AtomicFileAdapter} from "../adapter/file/AtomicFileAdapter";
 
 export interface JsonDBConfig {
-  filename: string,
-  saveOnPush: boolean,
-  humanReadable: boolean,
-  separator: string,
-  syncOnSave: boolean
+    readonly adapter: IAdapter<any>,
+    readonly saveOnPush: boolean,
+    readonly humanReadable: boolean,
+    readonly separator: string,
+    readonly syncOnSave: boolean
 }
 
 export class Config implements JsonDBConfig {
-  filename: string
-  humanReadable: boolean
-  saveOnPush: boolean
-  separator: string
-  syncOnSave: boolean
+    adapter: IAdapter<any>;
+    public readonly filename: string
+    humanReadable: boolean
+    saveOnPush: boolean
+    separator: string
+    syncOnSave: boolean
 
-  constructor(filename: string, saveOnPush: boolean = true, humanReadable: boolean = false, separator: string = '/', syncOnSave: boolean = false) {
-    this.filename = filename
+    constructor(filename: string, saveOnPush: boolean = true, humanReadable: boolean = false, separator: string = '/', syncOnSave: boolean = false) {
+        this.filename = filename
 
-    // Force json if no extension
-    if (path.extname(filename) === "") {
-      this.filename += ".json"
+        // Force json if no extension
+        if (path.extname(filename) === "") {
+            this.filename += ".json"
+        }
+
+        this.humanReadable = humanReadable
+        this.saveOnPush = saveOnPush
+        this.separator = separator
+        this.syncOnSave = syncOnSave
+        this.adapter = new JsonAdapter(new AtomicFileAdapter(this.filename));
     }
+}
 
-    this.humanReadable = humanReadable
-    this.saveOnPush = saveOnPush
-    this.separator = separator
-    this.syncOnSave = syncOnSave
-  }
+export class ConfigWithAdapter implements JsonDBConfig {
+    readonly adapter: IAdapter<any>;
+    readonly humanReadable: boolean;
+    readonly saveOnPush: boolean;
+    readonly separator: string;
+    readonly syncOnSave: boolean;
+
+
+    constructor(adapter: IAdapter<any>, humanReadable: boolean, saveOnPush: boolean, separator: string, syncOnSave: boolean) {
+        this.adapter = adapter;
+        this.humanReadable = humanReadable;
+        this.saveOnPush = saveOnPush;
+        this.separator = separator;
+        this.syncOnSave = syncOnSave;
+    }
 }
