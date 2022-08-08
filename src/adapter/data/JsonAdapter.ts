d7a904e84e6a5c3671d3177508faca217c0c909b
Antoine Aflalo <git@aaflalo.me>
2022-08-08 17:22:35 -0400

    feat(Date): Add support for serializing and deserializing date type
    Fixes #362

--- src/adapter/data/JsonAdapter.ts
+++ src/adapter/data/JsonAdapter.ts
@@ -4,11 +4,23 @@ export class JsonAdapter implements IAdapter<any> {
 
     private readonly adapter: IAdapter<string>;
     private readonly humanReadable: boolean;
+    private readonly dateRegex = new RegExp('^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}', 'm')
 
 
     constructor(adapter: IAdapter<string>, humanReadable: boolean = false) {
         this.adapter = adapter;
         this.humanReadable = humanReadable;
+    }
+
+    private replacer(key: string, value: any): any {
+        return value;
+    }
+
+    private reviver(key: string, value: any): any {
+        if (typeof value == "string" && this.dateRegex.exec(value) != null) {
+            return new Date(value);
+        }
+        return value;
     }
 
     async readAsync(): Promise<any> {
@@ -17,15 +29,15 @@ export class JsonAdapter implements IAdapter<any> {
             await this.writeAsync({});
             return {};
         }
-        return JSON.parse(data);
+        return JSON.parse(data, this.reviver.bind(this));
     }
 
     writeAsync(data: any): Promise<void> {
         let stringify = '';
         if (this.humanReadable) {
-            stringify = JSON.stringify(data, null, 4)
+            stringify = JSON.stringify(data, this.replacer.bind(this), 4)
         } else {
-            stringify = JSON.stringify(data)
+            stringify = JSON.stringify(data, this.replacer.bind(this))
         }
         return this.adapter.writeAsync(stringify);
     }
