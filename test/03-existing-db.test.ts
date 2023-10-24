96102ebd57e2198d531e785266d4856bfb322966
TonyT <antony.trupe@gmail.com>
2023-10-24 00:32:16 -0400

    made existing db tests atomic and not alter the test db

--- test/03-existing-db.test.ts
+++ test/03-existing-db.test.ts
@@ -1,17 +1,20 @@
-import {JsonDB} from '../src/JsonDB'
+import { JsonDB } from '../src/JsonDB'
 import { Config } from "../src/lib/JsonDBConfig"
 
 describe('JsonDB', () => {
-    const db = new JsonDB(new Config('test/game_file', true, true));
     describe('check existing database file', () => {
+        let db
+        beforeEach(() => {
+            db = new JsonDB(new Config('test/game_file', false, true));
+        })
         test('should contains data', async () => {
-            const role = {name: 'test:'}
+            const role = { name: 'test:' }
             expect(await db.getData(`/${role.name}`)).toBe('488722677205303327')
         })
 
         test('should delete data', async () => {
             await db.push('/deleteme:', 'abc')
-            const role = {name: 'deleteme:'}
+            const role = { name: 'deleteme:' }
             await db.delete(`/${role.name}`)
             expect(await db.exists(`/${role.name}`)).toBeFalsy()
         })
